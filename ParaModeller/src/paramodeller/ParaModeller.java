f443f9f08815d7cc09b682c23e4bb2c420cd879b
hghowe <hghowe@gmail.com>
2014-05-31 23:49:09 -0500

    Created constructor - generates the points needed to make a cylinder.

--- ParaModeller/src/paramodeller/ParaModeller.java
+++ ParaModeller/src/paramodeller/ParaModeller.java
@@ -6,17 +6,47 @@
 
 package paramodeller;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author harlan.howe
  */
 public class ParaModeller {
 
+    private ArrayList<DoubleVector3> myPoints;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ParaModeller app = new ParaModeller();
     }
     
+    public ParaModeller()
+    {
+        myPoints = new ArrayList<DoubleVector3>();
+        myPoints.add(DoubleVector3.generateZero());
+        for (int i=0; i<40; i++)
+        {
+            double angle = Math.PI*i/20;
+            myPoints.add(new DoubleVector3(12*Math.cos(angle),12*Math.sin(angle),0));
+        }
+        double phase = Math.PI*0.5/20;
+        for (int i=0; i<40; i++)
+        {
+            double angle = Math.PI*i/20;
+            myPoints.add(new DoubleVector3(12*Math.cos(angle+phase),12*Math.sin(angle+phase),10));
+        }
+        DoubleVector3 topPoint = DoubleVector3.generateZero();
+        topPoint.setZ(10);
+        myPoints.add(topPoint);
+        
+        int i=0;
+        for (DoubleVector3 p:myPoints)
+        {
+            System.out.println(i+"\t"+p);
+            i++;
+        
+        }
+    }
 }
