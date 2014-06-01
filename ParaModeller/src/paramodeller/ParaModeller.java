1e2b893f0159be37435de4e874fd897f7051968b
hghowe <hghowe@gmail.com>
2014-06-01 00:35:49 -0500

    Successful cylinder STL file created

--- ParaModeller/src/paramodeller/ParaModeller.java
+++ ParaModeller/src/paramodeller/ParaModeller.java
@@ -6,6 +6,9 @@
 
 package paramodeller;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
@@ -15,6 +18,7 @@ import java.util.ArrayList;
 public class ParaModeller {
 
     private ArrayList<DoubleVector3> myPoints;
+    private ArrayList<STLTriangle> myTriangles;
     /**
      * @param args the command line arguments
      */
@@ -25,6 +29,7 @@ public class ParaModeller {
     public ParaModeller()
     {
         myPoints = new ArrayList<DoubleVector3>();
+        myTriangles = new ArrayList<STLTriangle>();
         myPoints.add(DoubleVector3.generateZero());
         for (int i=0; i<40; i++)
         {
@@ -41,12 +46,42 @@ public class ParaModeller {
         topPoint.setZ(10);
         myPoints.add(topPoint);
         
-        int i=0;
-        for (DoubleVector3 p:myPoints)
+        for (int i=1;i<40;i++)
         {
-            System.out.println(i+"\t"+p);
-            i++;
-        
+            myTriangles.add(new STLTriangle(myPoints.get(0),myPoints.get(i),myPoints.get(i+1)));
         }
+        myTriangles.add(new STLTriangle(myPoints.get(0),myPoints.get(39),myPoints.get(1)));
+        for (int i=1;i<40;i++)
+        {
+            myTriangles.add(new STLTriangle(myPoints.get(i),myPoints.get(i+1),myPoints.get(i+40)));
+            myTriangles.add(new STLTriangle(myPoints.get(i+1),myPoints.get(i+41),myPoints.get(i+40)));
+        }
+        myTriangles.add(new STLTriangle(myPoints.get(40),myPoints.get(1),myPoints.get(80)));
+        myTriangles.add(new STLTriangle(myPoints.get(1),myPoints.get(41),myPoints.get(80)));
+        
+        for (int i=41; i<80; i++)
+            myTriangles.add(new STLTriangle(myPoints.get(81),myPoints.get(i),myPoints.get(i+1)));
+        myTriangles.add(new STLTriangle(myPoints.get(81),myPoints.get(80),myPoints.get(41)));
+        
+//        int i=0;
+//        for (DoubleVector3 p:myPoints)
+//        {
+//            System.out.println(i+"\t"+p);
+//            i++;
+//        
+//        }
+        try
+        {
+            PrintWriter fileout = new PrintWriter(new FileWriter("CylinderTest.stl")); 
+            fileout.println("solid cylinder");
+            for (STLTriangle stlt: myTriangles)
+                fileout.print(stlt);
+            fileout.println("endsolid");
+            fileout.close();
+        }catch (IOException ioe)
+        {
+            System.out.println("File not found.");
+        }
+        
     }
 }
